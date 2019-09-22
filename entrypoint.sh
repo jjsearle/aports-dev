2bd66ea8f6071c6245bc0c48141ced3df0e47dd9
joe <joe@dmcip.com>
2019-09-22 10:26:57 +0100

    Load any git hooks

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/ash
 su dev -c "/usr/bin/git config --global user.name \"$FULLNAME\""
 su dev -c "/usr/bin/git config --global user.email \"$EMAIL\""
+su dev -c "/usr/bin/git config --local core.hooksPath .githooks"
 su dev -
