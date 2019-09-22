81a4af50bd112e1dae409bafd54861c0aa33c9f8
joe <joe@dmcip.com>
2019-09-22 10:47:15 +0100

    Set the CONTRIBUTOR and MAINTAINER variables

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,6 @@ RUN apk add --update --upgrade --no-cache --force-overwrite alpine-sdk git vim a
 RUN adduser -u 1000 -DG abuild dev
 RUN echo 'dev    ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers.d/dev
 RUN su dev -c 'abuild-keygen -an'
-RUN cat /home/dev/.abuild/abuild.conf > /etc/abuild.conf
 COPY /entrypoint.sh /
 WORKDIR /aports
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,6 @@
 su dev -c "/usr/bin/git config --global user.name \"$FULLNAME\""
 su dev -c "/usr/bin/git config --global user.email \"$EMAIL\""
 su dev -c "/usr/bin/git config --local core.hooksPath .githooks"
+su dev -c "/bin/echo PACKAGER=\'\"$FULLNAME <$EMAIL>\"\' >> /home/dev/.abuild/abuild.conf"
+su dev -c "/bin/echo MAINTAINER=\'\"$FULLNAME <$EMAIL>\"\' >> /home/dev/.abuild/abuild.conf"
 su dev -
